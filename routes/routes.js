4897117efb8e9f4cdb10d9cfffcf0ac076684558
Fabio <heloflyer@hotmail.com>
2019-05-14 16:53:52 -0700

    corrected routes by adding post

--- routes/routes.js
+++ routes/routes.js
@@ -1,22 +1,23 @@
 const fs = require('fs');
 const path = require('path');
+const axios = require('axios');
 
 module.exports = (app, fileLocation) => {
-    app.get(
-        '/api/sites/:site', (req, res) => {
-            let file = path.join(fileLocation, req.params.site + '.html');
-            console.log('site --------------------', file)
-            fs.readFile(file, (err, data) => {
-                if (err) {
-                    if (err.code === 'ENOENT') {
-                        console.error('file does not exist or path is incorrect');
-                        throw err;
-                    }
-                };
-                res.send(data)
-            })
-        }
-    );
+    // app.get(
+    //     '/api/sites/:site', (req, res) => {
+    //         let file = path.join(fileLocation, req.params.site + '.html');
+    //         console.log('site --------------------', file)
+    //         fs.readFile(file, (err, data) => {
+    //             if (err) {
+    //                 if (err.code === 'ENOENT') {
+    //                     console.error('file does not exist or path is incorrect');
+    //                     throw err;
+    //                 }
+    //             };
+    //             res.send(data)
+    //         })
+    //     }
+    // );
 
     app.get(
         '/api/sites/', (req, res) => {
@@ -37,5 +38,18 @@ module.exports = (app, fileLocation) => {
             res.send(results)
         });
 
-    
+        app.post("/api/sites", function (req, res) {
+            axios.get(req.body.url)
+                .then(function (response) {
+                    let fileDestination = path.join(fileLocation, `${req.body.name}.html`);
+                    fs.writeFile(fileDestination, response.data, (err) => {
+                        if (err) throw err;
+                    });
+                    res.send(200)
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    res.send(500)
+                });
+        });
 }
